aee1ce1bb1496bd7506f802a534e31953454f455
Varshini Jayasankar <89716804+VarshiniJais@users.noreply.github.com>
2023-02-18 22:32:15 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, from PES2UG20CS567\n";
+  cout << "Pipeline Successful\n";
+  return 0;
+}
